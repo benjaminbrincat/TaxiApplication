eba5b1e22094213070979e9550ad86330cfaf895
benjaminbrincat <66258202+benjaminbrincat@users.noreply.github.com>
2021-04-20 15:42:43 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaxiApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
